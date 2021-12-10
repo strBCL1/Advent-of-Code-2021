6fb5ce1aa96ec3858097429f3e54e5995d5b1700
strBCL1 <work.artem.popov@gmail.com>
2021-12-10 21:22:16 +0100

    Day 6: Part 2 solved

--- Day 6/Part 1/App.java
+++ Day 6/Part 1/App.java
@@ -1,61 +1,31 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Arrays;
 
 class Solution {
-    final private int AMOUNT_OF_DAYS = 256;
-    private ArrayList<Integer> numbers = new ArrayList<Integer>();
-
+    final private int AMOUNT_OF_DAYS = 18;
 
     public void readFromFile() throws IOException {
-        BufferedReader bf = new BufferedReader(new FileReader("/home/mertens/VSCode Workspace/Java Language/Advent-of-Code-2021/Day 6/Part 1/input.txt"));
-        String currentLine = "";
-        
-        while ((currentLine = bf.readLine()) != null) {
-            Matcher matcher = Pattern.compile("\\d+").matcher(currentLine);
+        BufferedReader bf = new BufferedReader(new FileReader("path/yo/input/file"));
+        long[] fishesAges = new long[10];
 
-            while (matcher.find()) {
-                int currentNumber = Integer.parseInt(matcher.group());
-                numbers.add(currentNumber);
-            }
+        for (String s : bf.readLine().split(",")) {
+            fishesAges[Integer.parseInt(s)]++;
         }
 
-        bf.close();
+        for (int currentDay = 0; currentDay < AMOUNT_OF_DAYS; ++currentDay) {
+            fishesAges[7] += fishesAges[0];
+            fishesAges[9] = fishesAges[0];
 
-        countLanternfishes();
-    }
-
-
-    private void countLanternfishes() {
-        int currentDay = 0;
-        int currentSize = numbers.size();
-        int currentIndex = 0;
-
-        while (currentDay <= AMOUNT_OF_DAYS) {
-            if (currentIndex == currentSize) {
-                currentSize = numbers.size();
-                currentIndex = 0;
-                currentDay += 1;
-
-                if (currentDay == AMOUNT_OF_DAYS) {
-                    System.out.println(numbers.size());
-                    return ;
-                }
+            for (int i = 0; i < 9; ++i) {
+                fishesAges[i] = fishesAges[i + 1];
             }
 
-            numbers.set(currentIndex, numbers.get(currentIndex) - 1);
-
-            if (numbers.get(currentIndex) < 0) {
-                numbers.set(currentIndex, 6);
-                numbers.add(8);
-            }
-
-            currentIndex++;
-            
+            fishesAges[9] = 0;
         }
+
+        System.out.println(Arrays.stream(fishesAges).sum());
     }
 }
 
