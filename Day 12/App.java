a1d8d8cff3e365d0ea90f674eeb93e0a8e7170ca
strBCL1 <work.artem.popov@gmail.com>
2021-12-30 22:14:49 +0100

    Day 12: Part 1 code refactored

--- Day 12/App.java
+++ Day 12/App.java
@@ -1,5 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,22 +7,56 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/*
+Algorithm of input processing:
+    For each line in input file:
+        Initialize firstNode as a first half of line
+        Initialize secondNode as a second half of line
+
+        Add secondNode to firstNode's adjacency list
+        Add firstNode to secondNode's adjacency list
+    
+    Mark all small caves as unvisited before DFS traversal
+
+    For each neighbour of "start" node:
+        Start DFS traversal from current neighbour with a SHALLOW COPY of visitedNodes to save memory
+
+
+Algorithm of DFS(currentNode, visitedNodes, currentPath):
+    Add current node to current path
+
+    If current node is "end":
+        Add current path to set of paths
+        Print the current path
+        Return to caller node
+
+    Else if current node is small cave:
+        Mark current node as visited (small caves can only be visited once)
+
+    For each neighbour of current node:
+        If neighbour isn't "start" node && neighbour isn't visited or is a big cave:
+            Call DFS on that neighbour with a DEEP COPY of visited nodes (visited nodes may be different for each path)
+*/
 
 class Day12 {
-    private static Map<String, ArrayList<String>> adjacencyList = new HashMap<>();
-    private static Set<String> paths = new HashSet<>(); // Set of distinct paths
+    private static Map<String, ArrayList<String>> adjacencyList = new HashMap<>(); // Node -> its adjacent nodes
+    private static Set<String> paths = new HashSet<>(); // Contains distinct paths
 
 
+    /**
+     * Reads nodes from text file, creates adjacency list and traverses it using DFS traversal.
+     */
     public void readFromFile() {
-        try (BufferedReader br = new BufferedReader(new FileReader("/home/mertens/VSCode Workspace/Java Language/Advent-of-Code-2021/Day 12/input.txt"))) {
-            String currentLine = "";
-            
-            while ((currentLine = br.readLine()) != null) {
-                String[] halfes = currentLine.split("-");
+        try (Stream<String> inputStream = Files.lines(Paths.get(Day12.class.getResource("input.txt").toURI()))) {
+            inputStream.forEach(line -> {
+                String[] halfes = line.split("-");
                 String firstNode = halfes[0];
                 String secondNode = halfes[1];
 
+                // If first node isn't in adjacency list, it is put into adj. list. 
+                // Only AFTER FIRST NODE'S ADDITION second nodes is added to first node's adjacency list.
                 if (adjacencyList.putIfAbsent(firstNode, new ArrayList<>(List.of(secondNode))) != null) {
                     adjacencyList.get(firstNode).add(secondNode);
                 }
@@ -30,18 +64,21 @@ class Day12 {
                 if (adjacencyList.putIfAbsent(secondNode, new ArrayList<>(List.of(firstNode))) != null) {
                     adjacencyList.get(secondNode).add(firstNode);
                 }
+            });
 
-            }
 
-
+            // Mark all lowercase nodes (small caves) as unvisited
             Map<String, Boolean> visitedNodes = adjacencyList.keySet().stream()
                                                 .filter(key -> key.equals(key.toLowerCase()) && !key.equals("start") && !key.equals("end"))
                                                 .collect(Collectors.toMap(key -> key, value -> false));
 
+            // Start DFS traversal from each neighbour of "start" cave
+            for (String neighbour : adjacencyList.get("start")) {
+                DFS(neighbour, visitedNodes, "start");
+            }
 
-            DFS("start", visitedNodes, "");
-
-            System.out.println(paths.size());
+            // Print part 1 answer
+            System.out.println("Total amount of paths: " + paths.size());
         }
 
         catch (Exception e) {
@@ -50,21 +87,18 @@ class Day12 {
     }
 
 
+    /**
+     * Performs DFS traversal of current node.
+     * @param currentNode name of current node
+     * @param visitedNodes list of small caves that are marked with visited or not
+     * @param currentPath path with current node added to it
+     */
     private void DFS(String currentNode, Map<String, Boolean> visitedNodes, String currentPath) {
-        if (!currentPath.isEmpty()) {
-            currentPath += "," + currentNode;
-        }
+        currentPath += "," + currentNode;
 
-        if (currentNode.equals("start")) {
-            for (String neighbour : adjacencyList.get("start")) {
-                DFS(neighbour, new HashMap<>(visitedNodes), "start");
-            }
-            return ;
-        }
-
-        else if (currentNode.equals("end")) {
+        if (currentNode.equals("end")) {
             paths.add(currentPath);
-            System.out.println(currentPath);
+            System.out.println("Current path: " + currentPath);
             return ;
         }
 
