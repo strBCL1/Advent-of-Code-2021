9fe395e72fc4ae9709020235aaaa6896e6b3d218
strBCL1 <work.artem.popov@gmail.com>
2021-12-30 19:35:55 +0100

    Day 12 solved

--- Day 12/App.java
+++ Day 12/App.java
@@ -45,50 +45,8 @@ DFS(stack, visited, currentPath):
             DFS(stack, visited, currentPath)
 */
 
-/* Algorithm part 2:
-DFS(stack, visitedAmount, currentPath, presentVisitedTwice):
-    Если стак пустой:
-        Выписать кол-во путей
-        Вернуть
-
-    Текущая вершина = стек.pop()
-
-    Если текущая вершина == "старт":
-        Для всех соседей "старт": 
-            Добавить соседа в стак
-            DFS(stack, visitedAmount, "start", false)
-        Вернуть
-
-    Добавить текущую вершину в текущий путь
-
-    Если текущая вершина == "конец":
-        Выписать текущий путь
-        Добавить текущий путь в список путей
-        DFS(stack, visitedAmount, currentPath, presentVisitedTwice)
-
-    Если текущая вершина маленькая:
-        Кол-во посещений текущей вершины += 1
-
-        // Если вышел на самого себя (вершину, посещенную более 2-х раз) - вернуть
-        Если presentVisitedTwice == true && кол-во посещений текущей вершины > 2:
-            Вернуть
-
-        // Если кол-во посещений текущей вершины == 2, но еще нету вершины, посещенной два раза - сделать текущую вершину посещенной два раза
-        Если presentVisitedTwice == false && кол-во посещений текущей вершины == 2:
-            presentVisitedTwice = true
-        
-        Если кол-во посещений текущей вершины == 1:
-            Пометить текущую вершину как посещенную
-
-
-    Для всех соседей текущей вершины:
-        Если сосед != "старт" || сосед большой || сосед == "конец" || (сосед посещен и presentVisitedTwice == false):
-            Добавить соседа в стак
-            DFS(stack, visited, currentPath)
-*/
-
-
 /* Solution in pseudocode
+
 Adj. list:
 start = ['A', 'b']
 A = ['c', 'start', 'b', 'end']
@@ -135,30 +93,26 @@ class Day12 {
             }
 
 
-            // adjList.entrySet().forEach(e -> System.out.println(e.getKey() + " " + e.getValue())); System.out.println(); // Print adjacency list
-            // Map<String, Boolean> visited = adjList.keySet().stream()
-            //                                .filter(key -> !key.equals("start") && !key.equals("end") && key.equals(key.toLowerCase()))
-            //                                .collect(Collectors.toMap(key -> key, value -> false));
+            // adjList.entrySet().forEach(e -> System.out.println(e.getKey() + " " + e.getValue())); System.out.println();
+            Map<String, Boolean> visited = adjList.keySet().stream()
+                                           .filter(key -> !key.equals("start") && !key.equals("end") && key.equals(key.toLowerCase()))
+                                           .collect(Collectors.toMap(key -> key, value -> false));
 
             // visited.entrySet().forEach(e -> System.out.println(e.getKey() + " " + e.getValue())); // Print visited nodes
 
-            Map<String, Integer> visitedAmount = adjList.keySet().stream()
-                                                .filter(key -> !key.equals("start") && !key.equals("end") && key.equals(key.toLowerCase()))
-                                                .collect(Collectors.toMap(key -> key, value -> 0));
 
             Queue<String> stack = Collections.asLifoQueue(new ArrayDeque<>());
             stack.add("start");
-            DFS(stack, visitedAmount, "start", false);
+            DFS(stack, visited, "start");
         }
 
         catch (Exception e) {
             e.printStackTrace();
-            paths.forEach(System.out::println);
         }
     }
 
 
-    private void DFS(Queue<String> stack, Map<String, Integer> visitedAmount, String currentPath, boolean presentVisitedTwice) {
+    private void DFS(Queue<String> stack, Map<String, Boolean> visited, String currentPath) {
         if (stack.isEmpty()) {
             System.out.println("Amount of paths: " + paths.size());
             return ;
@@ -169,7 +123,7 @@ class Day12 {
         if (currentNode.equals("start")) {
             for (String neighbour : adjList.get("start")) {
                 stack.add(neighbour);
-                DFS(stack, new HashMap<>(visitedAmount), "start", false);
+                DFS(stack, new HashMap<>(visited), currentPath);
             }
             return ;
         }
@@ -179,28 +133,21 @@ class Day12 {
         if (currentNode.equals("end")) {
             System.out.println("Current path: " + currentPath);
             paths.add(currentPath);
-            DFS(stack, new HashMap<>(visitedAmount), currentPath, presentVisitedTwice);
+            DFS(stack, new HashMap<>(visited), currentPath);
             return ;
         }
 
         if (currentNode.equals(currentNode.toLowerCase())) {
-            visitedAmount.put(currentNode, visitedAmount.get(currentNode) + 1);
-            int visitedTimes = visitedAmount.get(currentNode);
-
-            if (presentVisitedTwice == true && visitedTimes > 2) {
+            if (visited.get(currentNode) == true) {
                 return ;
             }
-
-            if (presentVisitedTwice == false && visitedTimes == 2) {
-                presentVisitedTwice = true;
-            }
+            visited.put(currentNode, true);
         }
 
         for (String neighbour : adjList.get(currentNode)) {
-            if (!neighbour.equals("start") && (visitedAmount.getOrDefault(neighbour, 0) == 0 || neighbour.equals("end") || 
-                (visitedAmount.getOrDefault(neighbour, 1) == 1 && presentVisitedTwice == false))) {
-                    stack.add(neighbour);
-                    DFS(stack, new HashMap<>(visitedAmount), currentPath, presentVisitedTwice);
+            if (!neighbour.equals("start") && visited.getOrDefault(neighbour, false) == false) {
+                stack.add(neighbour);
+                DFS(stack, new HashMap<>(visited), currentPath);
             }
         }
     }
